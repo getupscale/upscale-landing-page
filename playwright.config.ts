0f35958facfc9179ee5484775a4c6011657c623e
dev-dami <damiade5banjo@gmail.com>
2026-02-28 09:31:47 +0100

    Add dependencies and Playwright setup

---
+++ playwright.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig, devices } from "@playwright/test";
+
+export default defineConfig({
+  testDir: "./e2e",
+  fullyParallel: true,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  use: {
+    baseURL: "http://127.0.0.1:3000",
+    trace: "on-first-retry"
+  },
+  webServer: {
+    command: "bun run dev --port 3000 --hostname 127.0.0.1",
+    url: "http://127.0.0.1:3000",
+    reuseExistingServer: !process.env.CI
+  },
+  projects: [
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] }
+    }
+  ]
+});
